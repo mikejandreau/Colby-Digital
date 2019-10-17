dfffd19f58a24a702a08fe2f83c7122d3c111b47
Mike Jandreau <mikejandreau@gmail.com>
2019-10-17 13:20:20 -0400

    Updating page styles and layout

--- front-page.php
+++ front-page.php
@@ -32,25 +32,59 @@ get_header();
     <?php /*
     */ ?>
 
+      <section class="title-block">
+        <div class="container">
+          <h1><?php echo get_the_title(); ?></h1>
+        </div>
+      </section>
+
+      <section class="intro-copy">
+        <div class="container">
+          <div class="content-dark-bg intro-copy-block">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+                the_content();
+            endwhile; else: ?>
+                <p>Sorry, no posts matched your criteria.</p>
+            <?php endif; ?>
+          </div>
+        </div>
+      </section>
+
+      <section class="triple-feature">
+        <div class="container">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-sm-4 triple-feature-block bg-image">
+                <a href="#" class="triple-feature-link">College Archives</a>
+              </div>
+              <div class="col-sm-4 triple-feature-block bg-image">
+                <a href="#" class="triple-feature-link">Scholarship</a>
+              </div>
+              <div class="col-sm-4 triple-feature-block bg-image">
+                <a href="#" class="triple-feature-link">Unique Collections</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+
     <div class="about-block">
         <div class="container">
 
 
 
+
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                    <!-- <header class="entry-header">
-                        <?php // the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-                    </header> -->
-                    <?php 
-                        while ( have_posts() ) : the_post();
-                        get_template_part( 'template-parts/content', 'page' );
-                            // // If comments are open or we have at least one comment, load up the comment template.
-                            // if ( comments_open() || get_comments_number() ) :
-                            // comments_template();
-                            // endif; 
-                        endwhile; // End of the loop.
-                    ?>
+
                 </div>
                 <!-- <div class="col-sm-4">
                     <?php // get_sidebar( new_england_vascular_access_template_base() ); ?>
@@ -77,6 +111,69 @@ get_header();
 <?php if ( !is_home() && is_front_page() ) : // If front page is set to show a static page, show this markup after content area ?>
 
 
+
+
+
+
+
+
+
+
+
+
+
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 mb-4 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="/img/colby-colbiana.jpg" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Colbiana</a>
+                </h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 mb-4 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="/img/colby-map.jpg" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Atlas of Maine</a>
+                </h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 mb-4 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="/img/colby-rubaiyat.jpg" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Rubaiyat</a>
+                </h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 mb-4 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.4s">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="/img/colby-music.jpg" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Sounds of Colby</a>
+                </h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum eaque. Non magni, voluptates quae, necessitatibus unde temporibus.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
     <section class="section blog-section">
         <div class="container">
 
@@ -87,18 +184,18 @@ get_header();
             */ ?>
             <div class="row">
 
-                <div class="col-sm-12 col-md-8">
+                <div class="col-sm-12">
                     <div class="row">
                                 
 
                         <?php
-                            $counter = 2; // Number of posts to pull
+                            $counter = 6; // Number of posts to pull
                             $recentPosts = new WP_Query(array(
                                 'showposts' => $counter, 
                                 'offset' => 0,  // Set this to 1 to skip over first post, 2 to skip the first two, etc.
                                 'order' => 'DESC', // Puts new posts first, to put oldest posts first, change to 'ASC'
                                 'post__not_in' => get_option("sticky_posts"), // Ignore sticky posts for this particular query
-                                'cat' => '-3', // Ignore events, which category ID=3
+                                // 'cat' => '-3', // Ignore events, which category ID=3
                             ));
                         ?>
 
